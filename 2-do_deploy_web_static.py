93ef46f98f10e255485443fcd583030ab5cbe275
dealdrain <dealdrain@yahoo.com>
2024-01-31 11:49:30 -0800

    2. Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+a Fabric script that distributes an archive to your web servers
+based on the file 1-pack_web_static.py
+"""
+from fabric.api import local, put, run, env
+from datetime import datetime
+
+env.user = 'ubuntu'
+env.hosts = ['100.27.14.242', '3.84.255.61']
+
+
+def do_pack():
+    """
+    doing project dir > packages
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('sudo mkdir -p ./versions')
+    path = './versions/web_static_{}'.format(now)
+    local('sudo tar -czvf {}.tgz web_static'.format(path))
+    name = '{}.tgz'.format(path)
+    if name:
+        return name
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    imp the zip packg
+    """
+    try:
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
+        put(archive_path, '/tmp')
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
+        print('New version deployed!')
+        return True
+    except:
+        return False
