8b400f8161d94ed8f3221a494bff58fb81e56927
dealdrain <dealdrain@yahoo.com>
2024-02-16 10:03:36 -0800

    correcting task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,90 +1,77 @@
 #!/usr/bin/python3
 """
-Database storage class
+Spells the db engine
 """
 
 
 from os import getenv
-from sqlalchemy import create_engine, MetaData
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 
-class DBStorage():
-    """Class for database storage"""
+class DBStorage:
+    """Repin a db engine"""
+
     __engine = None
     __session = None
 
     def __init__(self):
-        """Initializes storage"""
-        from models.base_model import Base
-        self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}:3306/{}'
-            .format(getenv("HBNB_MYSQL_USER"),
-                    getenv("HBNB_MYSQL_PWD"),
-                    getenv("HBNB_MYSQL_HOST"),
-                    getenv("HBNB_MYSQL_DB")),
-            pool_pre_ping=True)
+        """setting a fresh db"""
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns all objects of cls"""
-        from models.state import State
-        from models.city import City
-        from models.user import User
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        class_list = [
-            State,
-            City,
-            User,
-            Place,
-            Review,
-            Amenity
-        ]
-        rows = []
-        if cls:
-            rows = self.__session.query(cls)
+        """Query and return if cls is Nul"""
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
         else:
-            for cls in class_list:
-                rows += self.__session.query(cls)
-        return {type(v).__name__ + "." + v.id: v for v in rows}
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
-        """add object to db"""
-        if not obj:
-            return
+        """Add new obj to db session"""
         self.__session.add(obj)
 
     def save(self):
-        """commit changes to db"""
+        """Save amendment"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete obj from db"""
-        if obj:
+        """Del obj from db ses"""
+        if obj is not None:
             self.__session.delete(obj)
-            self.save()
 
     def reload(self):
-        """create all tables in the db"""
-        from models.base_model import Base
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.user import User
-        from models.review import Review
-        from models.place import Place
-        from sqlalchemy.orm import sessionmaker, scoped_session
-
-        if getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(self.__engine)
+        """Create a table in db comm ses"""
         Base.metadata.create_all(self.__engine)
-
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
-        """Thread specific storage"""
+        """exit ses"""
         self.__session.close()
