592ac585e3cc5588e8a0fc149428690a6cbe2bf0
dealdrain <dealdrain@yahoo.com>
2024-02-16 09:49:48 -0800

    correcting task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -14,5 +14,24 @@ def hello():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Add route /hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text=None):
+    """ inputed str 'replace _ for space and show text'"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text='is_cool'):
+    """input text 'replace _ for space and show text'"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
