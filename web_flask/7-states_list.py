5c0ecd1a73cb76ef7f0814e9d879af9cd68471a1
dealdrain <dealdrain@yahoo.com>
2024-02-16 10:40:30 -0800

    task8 correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """
-Starting a Flask web app.
+start flask
 """
 
 
 from models import storage
-from flask import Flask
-from flask import render_template
-
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Print html pg with list of states"""
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """RM SQLAlchemy session."""
+def appcontext_teardown(self):
+    """use storage for fetching data from the storage engine
+    """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/states_list', strict_slashes=False)
+def state_info():
+    """Display a HTML page inside the tag BODY"""
+    return render_template('7-states_list.html',
+                           states=storage.all(State))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
